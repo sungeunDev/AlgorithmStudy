3d902792a3fa070f9a9504486f0e42be6ad80d25
sungeunDev <starfg62@gmail.com>
2018-05-16 10:02:47 +0900

    [template] complete apple&orange template before function call

--- solved/HackerRank/AppleAndOrange/AppleAndOrange.swift
+++ solved/HackerRank/AppleAndOrange/AppleAndOrange.swift
@@ -1,3 +1,36 @@
 import Foundation
 
-print("apple and orange")
+// function declaration
+func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> Void {
+
+}
+
+let num = 5
+var input: [String] = []
+
+for _ in 0..<num {
+	let inputText = readLine()!
+	input.append(inputText)
+}
+
+// seperating variables from input
+func separate(str: String) -> [Int]{
+return str.split(separator: " ").map{ Int($0)! }
+}
+
+// variable declaration
+let s = separate(str: input[0])[0]
+let t = separate(str: input[0])[1]
+
+let a = separate(str: input[1])[0]
+let b = separate(str: input[1])[1]
+
+let m = separate(str: input[2])[0]
+let n = separate(str: input[2])[1]
+
+let apples: [Int]  = separate(str: input[3])
+let oranges: [Int] = separate(str: input[4])
+
+
+// function call
+countApplesAndOranges(s: s, t: t, a: a, b: b, apples: apples, oranges: oranges)
