b197f7c326cc62da7b2b919def0c3d1f16ec9314
sungeunDev <starfg62@gmail.com>
2018-05-24 20:59:26 +0900

    [update] theory - linked list

---
+++ 2.Theory/DataStructures/main.swift
@@ -0,0 +1,55 @@
+// Copyright (c) 2018 Razeware LLC
+// For full license & permission details, see LICENSE.markdown.
+
+public func example(of description: String, action: () -> Void) {
+    print("---Example of \(description)---")
+    action()
+    print()
+}
+
+example(of: "creating of linking nodes") {
+    let node1 = Node(value: 1)
+    let node2 = Node(value: 2)
+    let node3 = Node(value: 3)
+    
+    node1.next = node2
+    node2.next = node3
+    
+    print(node1)
+}
+
+example(of: "push") {
+    var list = LinkedList<Int>()
+    list.push(1)
+    list.push(2)
+    list.push(3)
+    
+    print(list)
+}
+
+example(of: "append") {
+    var list = LinkedList<Int>()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+    
+    print(list)
+}
+
+example(of: "inserting at a particular index") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print("Before inserting: \(list)")
+    var middleNode = list.node(at: 1)!
+    for i in 0...4 {
+        middleNode = list.insert(i, after: middleNode)
+    }
+    print("After insertin: \(list)")
+    
+    let poppedValue = list.pop()
+    print("After popping list: \(list)")
+    print("Popped value: " + String(describing: poppedValue))
+}
